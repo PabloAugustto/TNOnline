c1a4a0e77b60fa5f5fcf012534163ddbe9ca6581
PabloAugustto <pablo.augustto@hotmail.com>
2023-03-21 08:44:42 -0300

    menu-aside

---
+++ script.js
@@ -0,0 +1,6 @@
+function Abrirnav() {
+  document.getElementById('menu-aside').style.visibility = 'visible';
+}
+function Fecharnav() {
+  document.getElementById('menu-aside').style.visibility = 'hidden';
+}
